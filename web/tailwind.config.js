fb30ea85f99777147fb97651d69c58afb3bf146a
Pascal Bertschi <pascal@bertschi.io>
2020-09-19 23:43:59 +0200

    scan ts for class names

--- web/tailwind.config.js
+++ web/tailwind.config.js
@@ -1,15 +1,14 @@
 module.exports = {
   purge: {
     enabled: process.env.NODE_ENV === 'production',
-    content: ['./src/**/*.html', './src/**/*.scss'],
+    content: ['./src/**/*.html', './src/**/*.scss', './src/**/*.ts'],
   },
   future: {
     purgeLayersByDefault: true,
     removeDeprecatedGapUtilities: true,
   },
   theme: {
-    extend: {
-    },
+    extend: {},
   },
   variants: {},
   plugins: [],
