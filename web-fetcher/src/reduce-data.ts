690102a10ac5f29ef31fe642be48aade17138e7d
Pascal Bertschi <pascal@bertschi.io>
2020-09-19 16:36:31 +0200

    push category grouping

--- web-fetcher/src/reduce-data.ts
+++ web-fetcher/src/reduce-data.ts
@@ -8,6 +8,8 @@ const purchaseArticlesFile = path.join(
   'assets/purchase-articles.json'
 );
 
+function reduceArticle(article: any): { data: {}; file: string }[] {}
+
 async function main() {
   const purchaseArticles = readJson(purchaseArticlesFile);
   for (const purchaseId of Object.keys(purchaseArticles)) {
@@ -21,19 +23,76 @@ async function main() {
   writeJson(path.join(targetDir, 'purchase-articles.json'), purchaseArticles);
 
   const articlePath = path.join(process.cwd(), 'assets/articles');
+  const reducedArticles: { file: string; data: any }[] = [];
+  const categorySlugs: { [key: string]: any[] } = {};
+
   for (const file of fs.readdirSync(articlePath)) {
     const article = readJson(path.join(articlePath, file));
-    writeJson(path.join(targetDir, 'articles', file), {
-      name: article.name,
-      regulated_description: article.regulated_description,
-      image: {
-        original: article.image.original,
+    let deepestCategory = null;
+    for (const category of article.categories || []) {
+      if (deepestCategory == null || deepestCategory.level < category.level) {
+        deepestCategory = category;
+      }
+    }
+    const reducedArticle = {
+      data: {
+        name: article.name,
+        categorySlug: deepestCategory?.slug,
+        regulated_description: article.regulated_description,
+        image: {
+          original: article.image.original,
+        },
+        price: article.price.item ? article.price.item.price : null,
+        kcal: article.nutrition_facts?.standard?.nutrients?.find(
+          (s) => s.code === 'PIM_NUT_ENERGIE'
+        )?.quantity_alternate,
       },
-      price: article.price.item ? article.price.item.price : null,
-      kcal: article.nutrition_facts?.standard?.nutrients?.find(
-        (s) => s.code === 'PIM_NUT_ENERGIE'
-      )?.quantity_alternate,
-    });
+      file,
+    };
+    if (deepestCategory) {
+      if (!categorySlugs[deepestCategory.slug]) {
+        categorySlugs[deepestCategory.slug] = [];
+      }
+      categorySlugs[deepestCategory.slug].push(reducedArticle);
+    }
+    reducedArticles.push(reducedArticle);
+  }
+
+  for (const categorySlug of Object.keys(categorySlugs)) {
+    let priceMin = Number.MAX_VALUE;
+    let priceMax = Number.MIN_VALUE;
+
+    for (const article of categorySlugs[categorySlug]) {
+      if (article.data.price === null || article.data.price === undefined) {
+        continue;
+      }
+      if (article.data.price > priceMax) {
+        priceMax = article.data.price;
+      }
+      if (article.data.price < priceMin) {
+        priceMin = article.data.price;
+      }
+    }
+
+    const priceRange = priceMax - priceMin;
+    for (const article of categorySlugs[categorySlug]) {
+      if (article.data.price === null || article.data.price === undefined) {
+        continue;
+      }
+
+      article.data.priceRange = priceRange;
+      article.data.priceMax = priceMax;
+      article.data.priceMin = priceMin;
+      article.data.priceScore =
+        1 - (article.data.price - priceMin) / priceRange;
+    }
+  }
+
+  for (const reducedArticle of reducedArticles) {
+    writeJson(
+      path.join(targetDir, 'articles', reducedArticle.file),
+      reducedArticle.data
+    );
   }
 }
 
