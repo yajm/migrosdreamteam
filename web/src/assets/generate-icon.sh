24bd64289ae0756c6ad7180e8ecb8a449b39caf9
Pascal Bertschi <pascal@bertschi.io>
2020-09-19 13:17:18 +0200

    update name and logo of pwa

---
+++ web/src/assets/generate-icon.sh
@@ -0,0 +1,8 @@
+convert -resize 72x72 icons/icon.png PNG8:icons/icon-72x72.png
+convert -resize 96x96 icons/icon.png PNG8:icons/icon-96x96.png
+convert -resize 128x128 icons/icon.png PNG8:icons/icon-128x128.png
+convert -resize 144x144 icons/icon.png PNG8:icons/icon-144x144.png
+convert -resize 152x152 icons/icon.png PNG8:icons/icon-152x152.png
+convert -resize 192x192 icons/icon.png PNG8:icons/icon-192x192.png
+convert -resize 384x384 icons/icon.png PNG8:icons/icon-384x384.png
+convert -resize 512x512 icons/icon.png PNG8:icons/icon-512x512.png
