6674c4fd4cc1ed0e775ace1ee41ccbe2c337c08f
Pascal Bertschi <pascal@bertschi.io>
2020-09-19 08:32:42 +0200

    switch to light icons

--- web/src/app/app.component.ts
+++ web/src/app/app.component.ts
@@ -1,14 +1,26 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {faProductHunt} from '@fortawesome/free-brands-svg-icons';
-import {faCog, faCogs, faShoppingCart} from '@fortawesome/free-solid-svg-icons';
+import {faCog, faCogs, faShoppingCart, faUtensils} from '@fortawesome/pro-light-svg-icons';
+import {SwUpdate} from '@angular/service-worker';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  productIcon = faProductHunt;
+export class AppComponent implements OnInit {
+  productIcon = faUtensils;
   purchaseIcon = faShoppingCart;
   settingIcon = faCog;
+
+  constructor(private swUpdate: SwUpdate) {
+  }
+
+  ngOnInit(): void {
+    this.swUpdate.available.subscribe(async (evt) => {
+      console.log('update available');
+      //         window.location.reload();
+    });
+
+  }
 }
--- web/src/app/modules/search-input/components/search-input/search-input.component.ts
+++ web/src/app/modules/search-input/components/search-input/search-input.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {faSearch} from '@fortawesome/free-solid-svg-icons';
+import {faSearch} from '@fortawesome/pro-light-svg-icons';
 
 @Component({
   selector: 'app-search-input',
